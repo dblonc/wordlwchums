e18f533213fb88e56c8c4ef33734a543062e54d7
Daniel Bloncourt <dblonc@aol.com>
2022-02-11 20:58:06 -0500

    begin user auth

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+    
+
 
 app.get("/", (req, res) => res.send("Hello Wordl"));
 
-
-const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use("/api/users", users);
+
