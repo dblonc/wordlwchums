6d176e66e71945f3030912772f760466b14f8fb0
Daniel Bloncourt <dblonc@aol.com>
2022-02-14 16:42:42 -0500

    fixed auths

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,6 +11,9 @@ const validateLoginInput = require('../../validation/login');
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.post('/register', (req, res) => {
+
+    const { errors, isValid } = validateRegisterInput(req.body);
+
     if (!isValid) {
         return res.status(400).json(errors);
     };
@@ -18,12 +21,14 @@ router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
-                return res.status(400).json({ email: "A user has already registered with this address" })
+                errors.email = "Email already exists";
+                return res.status(400).json(errors)
             } else {
                 const newUser = new User({
                     username: req.body.username,
                     email: req.body.email,
-                    password: req.body.password
+                    password: req.body.password,
+                    password2: req.body.password2
                 })
 
                 bcrypt.genSalt(10, (err, salt) => {
@@ -43,6 +48,8 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res)=>{
 
+    const { errors, isValid } = validateLoginInput(req.body);
+
     if (!isValid) {
         return res.status(400).json(errors);
     }
@@ -53,7 +60,8 @@ router.post('/login', (req, res)=>{
     User.findOne({email})
     .then(user => {
         if(!user){
-            return res.status(404).json({email: "This user does not exist"});
+            errors.email = "User not found"
+            return res.status(404).json(errors);
         }
 
         bcrypt.compare(password, user.password)
